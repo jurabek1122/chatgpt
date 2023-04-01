e913aaa94b9d0d2440c8b8872d21e3ed5bf73894
jurabek1122 <jurabekkarimjonov89@gmail.com>
2023-04-01 11:35:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { OpenAIApi, Configuration } from 'openai'
+import user from './images/user.png'
+import ai from './images/ai.png'
 
 function App() {
+  const [value, setValue] = useState('')
+  const [chat, setChat] = useState([])
+  const [btnLoader, setBtnLoader] = useState(false)
+
+  const openai = new OpenAIApi(new Configuration({
+    apiKey: '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'
+  }))
+
+  const sendReq =  (e) => {
+    e.preventDefault();
+    setChat(prev => ([
+      ...prev, {
+        type: 'user',
+        content: value
+      }
+    ]))
+    setValue('')
+    setBtnLoader(true)
+    
+    openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{role: 'user', content: value}]
+    }).then(function(response) {
+      console.log(response, 'res')
+      setBtnLoader(false)
+      setChat(prev => ( [
+        ...prev, {
+          type: 'gpt',
+          content: response.data.choices[0].message.content
+        }
+      ]))
+    }).catch(function(error) {
+      console.error(error);
+      setBtnLoader(false)
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='chatWrapper'>
+      <div className='chat'>
+        {chat?.map((item, i) => <div key={i} className='message'>
+          {item.type === 'user' ? <img src={user} /> : <img src={ai} />}
+          <p>{item.content}</p>
+        </div>)}
+      </div>
+      <form className='forms' onSubmit={sendReq}>
+        <input 
+         type='text'
+         placeholder='savol bering'
+         value={value}
+         onChange={(e) => setValue(e.target.value)}
+         required
+        />
+        {btnLoader ? <button type='button' disabled>Javobni kuting ...</button> : <button type='submit'>send</button>}
+        
+      </form>
     </div>
   );
 }
